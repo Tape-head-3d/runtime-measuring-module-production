8358fe05393bbf9a3ded0ab7c1cf68b55676968c
Thimira <tapehead3d@gmail.com>
2024-10-17 19:02:01 +0530

    Automated commit at 2024-10-17 19:02:01

--- auto_push.sh
+++ auto_push.sh
@@ -12,4 +12,4 @@ CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 # Stage all changes, commit them with a timestamp, and push to GitHub
 git add .
 git commit -m "Automated commit at $CURRENT_TIME"
-git push https://Tape-head-3d:${GITHUB_TOKEN}@github.com/Tape-head-3d/Runtime-Measuring-Module.git
+git push https://Tape-head-3d:${GITHUB_TOKEN}@github.com/Tape-head-3d/runtime-measuring-module-production.git
